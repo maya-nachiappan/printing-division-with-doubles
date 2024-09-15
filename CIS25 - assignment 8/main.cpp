da084a261a7b0706e9462bf3225abacd3e98fb7b
maya-nachiappan <maya.n102304@gmail.com>
2024-09-15 05:54:12 -0700

    initial without double

--- CIS25 - assignment 8/main.cpp
+++ CIS25 - assignment 8/main.cpp
@@ -1,14 +1,19 @@
-//
-//  main.cpp
+
 //  CIS25 - assignment 8
 //
 //  Created by maya nachiappan on 9/15/24.
-//
+// program to print values in doubles
 
 #include <iostream>
+using namespace std;
+ 
+int main()
+{
+    int x=10;
+    int y=3;
+    double z= x/y;
+    cout <<z;
+   return 0;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
 }
+
